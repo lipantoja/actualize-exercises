0d1379165f20df5a56295ca1520dbbe71205762f
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-07-22 14:35:10 -0400

    Update 112.rb

---
+++ 112.rb
@@ -0,0 +1,18 @@
+# QUESTION 1
+# Write code to print hellohellohello using string addition.
+puts "hello" + "hello" + "hello"
+# Then write code to print hellohellohello again, this time using string multiplication.
+puts "hello" * 3
+
+# QUESTION 2
+# Read the code below and predict which lines will crash. 
+puts 9 + 3 # Output 12
+puts "9" + "3" # Output 93
+puts 9 - 3 # Output 6
+puts 9 * 3 # Output 27
+puts "9" * 3 # 999
+puts "3" * 9 # 333333333
+puts 9 / 3 # Output 3
+
+
+# Then run the code and remove the invalid lines one at a time, until only the valid code remains.
