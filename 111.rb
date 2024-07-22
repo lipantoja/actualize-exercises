6b032edc544540a2f7e3a3b8b6c06ab5ca29e0e2
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-07-22 13:08:46 -0400

    Update 111.rb

---
+++ 111.rb
@@ -0,0 +1,16 @@
+# QUESTION 1
+# Write code to print out the numbers 4, 30, 12, 983 on separate lines. 
+# For this exercise, you are required to use addition, subtraction, multiplication, and division at least once.
+
+puts 8 / 2
+puts 15 * 2
+puts 20 - 8
+puts 900 + 83
+
+# QUESTION 2
+# Write code using addition, subtraction, multiplication, or division with numbers that causes the computer to crash.
+# Add a comment with a copy of the error message.
+# Add a comment that describes the error message in your own words.
+
+Puts 30 # Does not throw an error for some reason?
+puts a 40 on this line # Error message states there is an error on line 16
