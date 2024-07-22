8d22979a20c5075d1ca055f00dd72a2057e52dbb
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-07-22 17:35:44 -0400

    Create 113.rb

---
+++ 113.rb
@@ -0,0 +1,21 @@
+# QUESTION 1
+# Write code to store 3 different numbers in three different variables.
+# Then write the code to print the three variables added together.
+first_number = 1
+second_number = 5
+third_number = 42
+
+puts first_number + second_number + third_number
+
+# QUESTION 2
+# If you run the code below, it will crash due to invalid variable names. 
+# Find the invalid variable names and delete the special characters 
+# (be sure to leave the special characters in the valid variable names alone).
+bob7 = 50
+bob = 50
+bob = 50
+bob = 50
+bob_ = 50
+_bob = 50
+bob = 50
+bob = 50
