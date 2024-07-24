faef0cbfaf2078420bb2d9e222884d7f7622366c
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-07-24 15:50:57 -0400

    Create 131.rb

---
+++ 131.rb
@@ -0,0 +1,18 @@
+# QUESTION 1
+# Write code using a while loop to print the phrase "Around the world" 144 times.
+rotation = 1
+while rotation <= 144
+    puts "Around the world"
+    rotation = rotation + 1
+end
+
+# QUESTION 2
+# The following code is supposed to print the numbers 1 to 10, but it currently doesn't work. 
+# Find and fix all the mistakes in the code.
+
+x = 1
+while x <= 10
+    puts x
+    x = x + 1
+end
+  
