732c0eeaf6ed7244d426f1b0871d3d55b39389d9
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-07-29 13:53:54 -0400

    Add exercise solutions

---
+++ 211.rb
@@ -0,0 +1,17 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# 5. Write a program that uses a variable to store a number, 
+# then prints the number times 10.
+
+number = 20
+puts number * 10
+
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
