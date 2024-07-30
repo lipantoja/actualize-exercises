83255f6346a995b5f81ab6011f5af04febdad23b
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-07-30 17:10:03 -0400

    Add exercise solutions

---
+++ 221.rb
@@ -0,0 +1,19 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# 6. Write a program that uses variables to store two words, 
+# then prints both words on the same line in all capital letters
+
+fruit = "apple"
+drink = " juice"
+
+fresh = fruit + drink
+puts fresh.upcase
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+#   It was was easy to pull the components I needed from memory, but forgot how to print on the same line without making the two strings a new variable
+#   I'm going to have to remember I can do the same action in the "fresh" variable but just in the 'puts' line
+
