31703e30a2ae0152cf83139892378942aa1568b5
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-07-31 14:12:54 -0400

    Adds exercise solutions

---
+++ 231.rb
@@ -0,0 +1,17 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# 01-variables1.md
+# 7. Write a program that uses a variable to store a word, 
+# then prints the number of letters in the word.
+
+word = "soup"
+puts word.length
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+    # Got this first try, will move on to item 8. regarding decimals to integers
+# If not, describe the part you got stuck on that requires more practice.
+
+
