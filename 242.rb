43b766c943e48a9ccb69a67d2eda4400ec95d5ae
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-08-01 18:16:38 -0400

    Add exercise solutions

---
+++ 242.rb
@@ -0,0 +1,15 @@
+# QUESTION 1
+# Use a variable to store an array with three different words.
+# Then add one more word to the array and print the array on one line.
+
+fruits = ["apple", "orange", "plum"]
+fruits.push("grape")
+p fruits
+
+# QUESTION 2
+# Use a variable to store a hash with two different words and their definitions.
+# Then add one more word with its definition and print the hash on one line.
+
+definitions = {"boop" => "A touch of a nose with your index finger", "snoot" => "The nose of an animal or cute creature"}
+definitions["water bottle"] = "A container used for drinking water on the go"
+p definitions
