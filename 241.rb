c33c7a5f81f9b4a2b56dcec32e4dfe93b3066c05
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-08-01 17:25:29 -0400

    Add exercise solutions

---
+++ 241.rb
@@ -0,0 +1,13 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# 8. Write a program that uses a variable to store a number with decimals, then prints the number as an integer. 
+
+pi = 3.1425
+puts pi.to_i
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
