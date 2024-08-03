8c10873e2153cdbca0e12909256ee990c08405ed
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-08-03 13:30:44 -0400

    Add exercise solutions

---
+++ 251.rb
@@ -0,0 +1,20 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# 02-conditionals
+# 1. Use a variable to store a number, then write a condition that 
+#    prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+number = 10
+if number == 10
+    p 0
+else
+    p -1
+end
+# Were you able to easily solve the problem from memory?
+#  Was easy to remember besides not adding an "end" at the very end.
+#  Would like start on item 10, as it seems more complex next time in this same list of conditionals
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
