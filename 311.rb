0df85cc5f3fc4dc075ff01c99e958dc837bba402
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-08-05 21:03:12 -0400

    Add exercise solutions

---
+++ 311.rb
@@ -0,0 +1,20 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+    # It almost feels like I did not at all learn about methods. I think methods are goint to take the most practice for me.
+
+
+# 1. Write a method that takes in a number and returns 
+# the number times two. 
+# Then run the method and print the result.
+
+def double(number)
+    return number * 2
+end
+
+pp double(10)
