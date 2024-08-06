a21b80d55aff2ab205dc5c0f1e1bd84a17ac9e2b
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-08-06 19:49:49 -0400

    Add exercise solutions

---
+++ 313.rb
@@ -0,0 +1,20 @@
+# QUESTION 1
+# Create a fortune teller which tells the user a fortune based on the user's favorite number. 
+# Give at least 3 possible outcomes. So along these lines, for example: 
+# If the user's favorite number is below 50, give fortune X. 
+# If the user's favorite number is between 50 and 100, give fortune Y. 
+# If the user's favorite number is above 100, give fortune Z.
+
+# Get input from user with ask string
+pp "Tell me your favorite number:"
+number = gets.chomp
+# use if statement for each of the fortunes
+if number <= 50
+    puts "I sense you will encounter a difficult discussion..."
+elsif number => 50 && <= 100
+    puts "I forsee peace and harmony in your future..."
+else
+    puts "I foresee a little wizard frog will pay you a visit"
+end
+
+pp number
---
+++ 321.rb
@@ -0,0 +1,20 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+    # I'm excited I think this is good practice for me, need to play with methods more.
+    # Realize I'm more familiar with the other tools because I've studied other languages in the past.
+    # I want to stay here and pratice more methods, I think other concepts make sense to me and will just take repetition
+
+
+# Write a method that takes in a string and returns the string 
+# with all capital letters. Then run the method and print the result.
+
+def big(word)
+    return word.to_s.upcase
+end
+pp big("gigantic")
