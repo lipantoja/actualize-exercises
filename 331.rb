b7664cf5a7318f5ff4cc9128ff7fbcd236349c9a
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-08-07 22:04:30 -0400

    Add exercise solution '
    '

---
+++ 331.rb
@@ -0,0 +1,52 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+    # was difficult at first but was able to work through it. will be working on loops and redoing loops for awhile
+
+
+# 1. Write a while loop to print the numbers 1 through 10.
+
+index = 0
+number = 0
+
+while index < 10
+    puts number = number + 1
+    index = index + 1
+end
+
+# 2. Write a while loop that prints the word "hello" 5 times.
+
+index = 0
+word = "hello"
+
+while index < 5
+    puts word
+    index = index + 1
+end
+
+# 3. Write a while loop that asks the user to enter a 
+# word and will run forever until the user enters the word "stop".
+
+puts "Enter a word:"
+word = gets.chomp
+
+while word != "stop"
+    puts "Enter a word:"
+    word = gets.chomp
+end
+
+# 4. Write a while loop that prints the numbers 0 through 100, 
+# increasing by 5 each time.
+
+
+number = 0
+
+while number < 100
+    puts number = number + 5
+end
+
