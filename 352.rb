09a0b264613cb11b1c61fd09c7e04e285122e39c
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-08-09 18:45:34 -0400

    Add exercise solutions

--- 352.rb
+++ 352.rb
@@ -3,6 +3,15 @@
 words = ["Big", "Elephants", "Can", "Always", "Understand", "Small", "Elephants"]
 
 
+# loop through the objects in the array
+# split the string
+# ? figure out how to grab the first letter - same as call to an array?
+# push the output of the array to 
 
+index = 0
+while index < 7
+    pp words[index].split(//)[0]
+    index = index + 1
+end
 
 
