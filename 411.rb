422a77223bc327856e14cfd8c41dfee0cdb199d5
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-08-12 19:52:32 -0400

    Add exercise solutions

---
+++ 411.rb
@@ -0,0 +1,25 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
+# 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+array = ["Hello", "there, ", "World"]
+
+# puts array.insert(3, "I " "am " "Laura.")
+
+# array << "I'm " "Laura."
+# puts array
+
+
+#2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+array = ["A", "B", "C", "D"]
+array[1] = 12
+puts array
