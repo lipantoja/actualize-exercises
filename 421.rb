ecf88a1323985f0b564e66b7b332ab827fd22ad7
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-08-13 19:50:29 -0400

    Add exercise solutions

---
+++ 421.rb
@@ -0,0 +1,28 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
+# 3. Make a hash to store prices for 3 different menu items. 
+# Then add a new menu item and price and print the hash to see the result.
+
+menu = {"Apple Pie" => 5, "Milkshake" => 4, "Burger" => 13}
+menu["Fries"] = 2
+puts menu
+
+# 4. Make a hash to store a 
+# book's title, author, number of pages, and language. 
+# Then print each attribute on separate lines.
+
+book = { "title" => "One Fish, Two Fish, Red Fish, Blue Fish", "author" => "Dr. Seuss", "pages" => 20, "language" => "English",
+}
+
+pp book["title"]
+pp book["author"]
+pp book["pages"]
+pp book["language"]
