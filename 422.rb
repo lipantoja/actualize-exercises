cfa119e0ad8cff4bbc13a2670a0e1e067c6d541a
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-08-13 18:28:04 -0400

    Add exercise solutions

---
+++ 422.rb
@@ -0,0 +1,10 @@
+# QUESTION 1
+# Write a method that takes in a string and a number n and returns the string repeated n times.
+# Then run the method and print the result.
+
+
+def rep_string(string, times)
+    string * times
+end
+
+puts rep_string("Testing ", 3)
