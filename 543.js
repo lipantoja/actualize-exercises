7076d1c1b04bdff4bce89ada4b6b098be18c2b34
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-08-24 15:42:36 -0400

    Add exercise solution

---
+++ 543.js
@@ -0,0 +1,24 @@
+// QUESTION 1
+// Write code to store an array of numbers.
+// The print out the last item in that array.
+
+
+// QUESTION 2
+// Write code to store a number in a variable.
+// Then write a condition that 
+// prints -1 if the number is less than 10, 
+// prints 1 if the number is greater than 10, 
+// and prints 0 if the number is equal to 10.
+
+
+var numbers = [1, 2, 3, 4, 5]
+console.log(numbers(4))
+
+var x = 11
+if (x < 10) {
+    console.log(-1);
+} else if (x == 10){
+    console.log(0)
+} else {
+    console.log(-1)
+}
