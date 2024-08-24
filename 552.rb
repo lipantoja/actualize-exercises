d35f3cf67d2fa0b975e9437db315e5a5ae64132c
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-08-24 17:19:58 -0400

    Add exercise solution

---
+++ 552.rb
@@ -0,0 +1,12 @@
+# QUESTION 1
+# Write a method that takes in three strings and uses string interpolation 
+# to return all three strings combined with spaces in between as a single string. 
+# Then run the method and print the result.
+
+
+name = "Slim Shady"
+level = "professional"
+occupation = "rapper"
+
+career = "Here we have #{name}, who has #{level} experience as a #{occupation}"
+puts career
